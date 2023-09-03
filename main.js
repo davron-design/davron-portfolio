1d54830df2e45fab146b98a27a93afe6b3e49afd
Davron <davron.marx@gmail.com>
2023-09-03 17:50:09 -0400

    added ScrollTrigger.normalizeScroll(true)

--- main.js
+++ main.js
@@ -35,6 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   gsap.registerPlugin(ScrollTrigger);
+  ScrollTrigger.normalizeScroll(true);
 
   //--LENIS
   // Lenis Selectors
@@ -135,6 +136,50 @@ document.addEventListener('DOMContentLoaded', function () {
   createSwiper('is-about');
   createSwiper('is-works');
   createSwiper('is-services');
+
+  //--> Stills Swiper
+  function stillsSwiper(className) {
+    return new Swiper(`.swiper.${className}`, {
+      // General Parameters
+      speed: 1000,
+      threshold: 5,
+      watchSlidesProgress: true,
+      slidesPerGroupAuto: false,
+      lazy: {
+        enabled: true,
+        loadPrevNext: true,
+        loadPrevNextAmount: 2,
+      },
+
+      // Navigation
+      grabCursor: true,
+      keyboard: { enabled: true },
+      pagination: {
+        clickable: true,
+        type: 'fraction',
+        dynamicBullets: true,
+        el: `.swiper-pagination.${className}`,
+      },
+      navigation: {
+        prevEl: `.swiper-arrow-prev.${className}`,
+        nextEl: `.swiper-arrow-next.${className}`,
+      },
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        pauseOnMouseEnter: true,
+        disableOnInteraction: false,
+        enabled: true,
+      },
+
+      // Effects
+      modules: [EffectPanorama],
+      parallax: { enabled: true },
+      effect: 'panorama',
+    });
+  }
+
+  stillsSwiper('is-stills');
 
   //--GSAP
 
