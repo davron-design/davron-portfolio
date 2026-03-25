b4fbcd79d68ed50f0952f51ee4b331c80990a535
Davron <104047680+davron-design@users.noreply.github.com>
2026-03-25 20:21:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /**
- * @version 1.0.9
+ * @version 1.0.9.1
  * @author davron-design.com
  */
 
 'use strict';
 console.log(
-  '%c Welcome to (formerly) D_DESIGN v1.0.9',
+  '%c Welcome to (formerly) D_DESIGN v1.0.9.1',
   'background: #ff5621; color: #121212; display: block; padding:5px; padding-right: 10px; border-radius:4px;',
 );
 
